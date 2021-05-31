741feabc5c42be87dc0e5714c01310278326218b
heliokles <heliokles@github.com>
2021-05-30 23:33:21 -0700

    submission

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from "./components/Character"
+
+const BASE_URL = "https://swapi.dev/api/"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +13,23 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [character, setCharacter] = useState([])
+
+  useEffect(() => {
+        axios.get(`${BASE_URL}people`)
+          .then((res) => {
+            console.log('API works hurrah', res);
+            setCharacter(res.data);
+          })
+          .catch(error => {
+            console.log("error in useEffect", error)
+          })
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Starwars Characters</h1>
+        <Character CharacterList={character}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,49 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+    background: black;
+    font-family: Roboto;
+    color: white;
+
+    text-align: left;
+    line-height: 15px;
+
+    margin: 35px;
+    border: 35px;
+    padding: 35px;
+`
+
+const StyledTitle = styled.div`
+    font-size: 36px;
+    padding: 50px;
+`
+
+const Styledprop = styled.div`
+    padding-left: 50px;
+
+`
+
+function Character (props) {
+
+    const { CharacterList } = props;
+
+    return (
+        <StyledCharacter>
+        {
+            CharacterList.map((item) => {
+                return (
+                    <div>
+                        <StyledTitle>{item.name}</StyledTitle>
+                        <Styledprop>Height: {item.height}</Styledprop>
+                        <Styledprop>Gender: {item.gender}</Styledprop>
+                    </div>
+                )
+            })
+        }
+        </StyledCharacter>
+    )
+}
+
+export default Character
